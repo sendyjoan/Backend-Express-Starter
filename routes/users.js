f05fb342fa487dced2dd5649dc26fa571edc5448
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-05-06 23:27:33 +0700

    Setting Router

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const userHandler = require("./handlers/users");
 const userIdHandler = require("./handlers/users/id");
 
-router.get('/', userHandler.get);
-router.post('/', userHandler.post);
-router.get('/:userId', userIdHandler.get);
-router.put('/:userId', userIdHandler.put);
-router.delete('/:userId', userIdHandler.delete);
+router.route("/")
+  .get(userHandler.get)
+  .post(userHandler.post);
+
+router.route("/:userId")
+  .get(userIdHandler.get)
+  .put(userIdHandler.put)
+  .delete(userIdHandler.delete);
 
 module.exports = router;
